40246d4dd1b3ee031094cffad19816bb01400195
Wojciech Trojanowski <w.trojanowski1@gmail.com>
2022-12-28 18:18:30 +0100

    Ready

---
+++ app.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('.card__img-input');
+const list = document.querySelector('.list');
+const li = document.querySelectorAll('li');
+let liArray = Array.from(li);
+let text = input.value;
+
+const search = (text) => {
+	liArray.forEach((item) => {
+		if (!item.textContent.toLowerCase().includes(text)) {
+			item.classList.add('hide');
+		} else {
+			item.classList.remove('hide');
+		}
+	});
+};
+
+const handleKeyboard = (e) => {
+	if (e.key == 'Backspace') {
+		text = text.split('');
+		text.pop();
+		text = text.join('');
+
+		search(text);
+	} else {
+		text += e.key;
+		search(text);
+	}
+};
+
+input.addEventListener('keyup', handleKeyboard);
